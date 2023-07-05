0063e00008a6ef9f00beb36c45292babb90a6a36
matim <mati.matias09@gmail.com>
2023-07-05 11:25:14 +0200

    multithreaded working

---
+++ src/main/java/ThreadedServer.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ThreadedServer implements Runnable {
+    Socket socket;
+
+    ThreadedServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
+        ) {
+            while (true) {
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    writer.write(message + "\n");
+                    writer.flush();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
