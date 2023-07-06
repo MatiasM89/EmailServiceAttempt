56b978ef575ca5b44bd584c64842d76d20ada0cf
matim <mati.matias09@gmail.com>
2023-07-06 12:03:55 +0200

    partial EmailSender

---
+++ src/main/java/Email/EmailSender.java
@@ -0,0 +1,38 @@
+package Email;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailSender {
+    public static void main(String[] args) {
+
+        String senderEmailAddress = "panboleslaw89@gmail.com";
+        String senderEmailPassword = "Arab21$%";
+        String recieveEmailAddress = "mati.matias09@gmail.com";
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.user", senderEmailAddress);
+        properties.put("mail.smtp.password", senderEmailPassword);
+
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmailAddress, senderEmailPassword);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmailAddress));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recieveEmailAddress));
+        } catch (MessagingException e) {
+            System.out.println("Exception with SenderAddress");
+        }
+
+    }
+}
