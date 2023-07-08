dae4dccda93a7d482e92e8fafc603189ee39c550
matim <mati.matias09@gmail.com>
2023-07-08 17:29:23 +0200

    GUI configuration

---
+++ src/main/java/AppFXTest/FXRun.java
@@ -0,0 +1,16 @@
+package AppFXTest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class FXRun extends Application {
+
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
