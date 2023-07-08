ba92c1d619b8ec1006113ef982abf341b2692656
matim <mati.matias09@gmail.com>
2023-07-08 12:51:18 +0200

    EmailViewer working, some improvements with EmailViewer

--- src/main/java/Email/EmailRun.java
+++ src/main/java/Email/EmailRun.java
@@ -2,6 +2,6 @@ package Email;
 
 public class EmailRun {
     public static void main(String[] args){
-        EmailViewer.receiveMail("panboleslaw89@gmail.com","xyipsudfbsbwftsx","pop.gmail.com");
+        EmailViewer.receiveMail("mail","pass","pop.gmail.com");
     }
 }
