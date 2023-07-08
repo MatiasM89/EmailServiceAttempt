bc4c48c034974a982160278fc755ee37c0d607b9
matim <mati.matias09@gmail.com>
2023-07-08 12:50:11 +0200

    EmailViewer working, some improvements with EmailViewer

---
+++ src/main/java/Email/EmailRun.java
@@ -0,0 +1,7 @@
+package Email;
+
+public class EmailRun {
+    public static void main(String[] args){
+        EmailViewer.receiveMail("panboleslaw89@gmail.com","xyipsudfbsbwftsx","pop.gmail.com");
+    }
+}
--- src/main/java/Email/EmailViewer.java
+++ src/main/java/Email/EmailViewer.java
@@ -1,12 +1,13 @@
 package Email;
 
-import com.sun.mail.pop3.POP3Folder;
-
 import javax.mail.Folder;
+import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Store;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.ArrayList;
 
 public class EmailViewer {
     public static void receiveMail(String EmailAddress, String EmailPassword, String hostNameAddress) {
@@ -14,17 +15,24 @@ public class EmailViewer {
         properties.put("mail.pop3.host", hostNameAddress);
         properties.put("mail.pop3.port", "995");
         properties.put("mail.pop3.ssl.enable", "true");
-        properties.put("mail.store.protocol", "pop3");
+        properties.put("mail.protocol.store", "pop3");
 
         Session session = Session.getDefaultInstance(properties);
         try {
-            Store storage = session.getStore("pops3");
+            Store storage = session.getStore("pop3s");
             storage.connect(hostNameAddress, EmailAddress, EmailPassword);
             Folder emailFolder = storage.getFolder("INBOX");
             emailFolder.open(1); //read-only
+            List<Message> messages = new ArrayList<>(Arrays.asList(emailFolder.getMessages()));
+            for(Message msg : messages){
+                System.out.println(msg.getSubject());
+            }
+
+            storage.close();
+            emailFolder.close();
 
         } catch (Exception e) {
-            System.out.println(e.getMessage() + " trace " + Arrays.toString(e.getStackTrace()));
+            System.out.println(e.getMessage());
         }
 
     }
