eef551743d5cd316e44c0ef6469799dd6f885774
matim <mati.matias09@gmail.com>
2023-07-13 13:00:58 +0200

    lots of work with main page

--- src/main/java/AppFXTest/LoginPageController.java
+++ src/main/java/AppFXTest/LoginPageController.java
@@ -1,14 +1,21 @@
 package AppFXTest;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
+import java.io.IOException;
 import java.util.Properties;
 
 public class LoginPageController {
@@ -19,7 +26,11 @@ public class LoginPageController {
     @FXML
     private Label credentialsLabel;
 
-    public void signUp() {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void signUp(ActionEvent event) throws IOException {
         String emailAddress = emailInput.getText();
         String password = passwordInput.getText();
         // login part
@@ -34,9 +45,18 @@ public class LoginPageController {
             storage.connect("pop.gmail.com", emailAddress, password);
         } catch (NoSuchProviderException e) {
             System.out.println(e.getMessage());
+            return;
         } catch (MessagingException e) {
             credentialsLabel.setVisible(true);
+            return;
         }
+
+        credentialsLabel.setVisible(false);
+        root = FXMLLoader.load(getClass().getResource("/FX/MainPage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
 }
---
+++ src/main/java/AppFXTest/MainPageController.java
@@ -0,0 +1,4 @@
+package AppFXTest;
+
+public class MainPageController {
+}
